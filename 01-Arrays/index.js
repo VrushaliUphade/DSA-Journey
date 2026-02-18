f5bb3d46cf15ce01b7d3a95486357643645b18ec
Vrushali Uphade <vrushaliuphade@gmail.com>
2026-02-18 22:43:40 +0530

    Day 5:More star patterns

--- 01-Arrays/index.js
+++ 01-Arrays/index.js
@@ -140,3 +140,105 @@
 //     }
 //     console.log(row); 
 // }
+
+
+// // 5.
+// // 1 2 3 4 5
+// // 1 2 3 4
+// // 1 2 3
+// // 1 2
+// // 1
+
+// let n = 5;
+// for(let i = 0;i<n;i++){
+//     let row = " ";
+//     for(let j =0 ;j<n-i;j++){
+//         row = row + (j+1);
+//     }
+//     console.log(row);
+    
+// }
+
+// // 6.
+// // * * * * *
+// // * * * *
+// // * * *
+// // * *
+// // *
+
+// let n = 5;
+// for(let i = 0;i<n;i++){
+//     let row = " ";
+//     for(j = 0;j<n-i;j++){
+//         row = row + "*"
+//     }
+// console.log(row);
+
+// }
+
+// // 7.
+// // _ _ _ _ *
+// // _ _ _ * *
+// // _ _ * * *
+// // _ * * * *
+// // * * * * *
+
+// let n =5;
+// for(let i = 0;i<n; i++){
+//     let row = " "
+//     for(let j = 0;j<n-(i+1);j++){
+//         row = row + "-"
+//     }
+//     for(let k = 0; k<i+1;k++){
+//         row = row + "*"
+ 
+//     }
+//     console.log(row);
+    
+// }
+ 
+////8.
+// //  1
+// //  10
+// //  101
+// //  1010
+// //  10101
+
+// let n = 5;
+// for(let i = 0;i<n;i++){
+//     let row = " ";
+//     let Switch = 1;
+//     for(let j = 0;j<i+1;j++){
+//         row = row + Switch
+//        if(Switch == 1){
+//         Switch = 0
+//        }
+//        else {
+//          Switch =1;
+//        }
+//     }
+//     console.log(row);   
+// }
+
+////9.
+//  //1
+//  //01
+//  //010
+//  //1010
+//  //10101
+
+// let n = 5;
+// let Switch = 1;
+// for(let i = 0;i<n;i++){
+//     let row = " ";
+//     for(let j = 0;j<i+1;j++){
+//         row = row + Switch
+//        if(Switch == 1){
+//         Switch = 0
+//        }
+//        else {
+//          Switch =1;
+//        }
+//     }
+//     console.log(row);   
+// }
