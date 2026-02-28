12c12dd8266dd2a94cf49bfb9be7ef58e30afcf0
Vrushali Uphade <vrushaliuphade@gmail.com>
2026-02-28 23:38:29 +0530

    Remove Zeros

--- 01-Arrays/index.js
+++ 01-Arrays/index.js
@@ -352,5 +352,21 @@
 // let res = reverseString(s);
 // console.log(res);
 
-
+// //6.Remove Zeros
+// let  removesZeros =function(arr){
+//  let x = 0;
+//  for(let i = 0;i<arr.length;i++){
+//     if(arr[i]!==0){
+//         arr[x]=arr[i]
+//         x++;
+//     }
+//  }
+//  for(let i = x;i<arr.length;i++){
+//     arr[i] = 0;
+//  }
+//  return arr;
+// }
+//   let arr = [0,2,3,0,1,9];
+//   let res = removesZeros(arr);
+//   console.log(res);
     
